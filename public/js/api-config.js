a30d10371c73fcc1e7b95c743299cab6418b7f30
kehinde-omotoso-78financials <110571589+kehinde-omotoso-78financials@users.noreply.github.com>
2025-02-20 11:22:12 +0100

    Added API Keys

--- public/js/api-config.js
+++ public/js/api-config.js
@@ -1,4 +1,6 @@
 // public/js/api-config.js
-export const TMDB_API_KEY = 'YOUR_TMDB_API_KEY'; // Replace with your actual TMDB API key
+export const TMDB_API_KEY = '47e76f674cadfda610012f66b0cc301b'; // Replace with your actual TMDB API key
 export const YELP_API_KEY = 'YOUR_YELP_API_KEY';   // Replace with your actual Yelp API key
 export const YELP_API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3'; // Using cors-anywhere for development - NOT for production!
+
+///TMDB TOKEN: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0N2U3NmY2NzRjYWRmZGE2MTAwMTJmNjZiMGNjMzAxYiIsIm5iZiI6MTc0MDA0NTcyMS4zODMwMDAxLCJzdWIiOiI2N2I2ZmQ5OWIzZjhmYjI3YzQxYTA1MWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3h_NG1Wx7D-XL1qo9en-006Rw8mvjRO58aTpuQmLFR0
